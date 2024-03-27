99936d234bd6ed7a93734fddef59e4cc27137254
João Vitor da Silva <joaovsilva3530@gmail.com>
2024-03-27 15:07:54 -0300

    Add screens

---
+++ Telas/PRONTOS/Tela Inicial.c
@@ -0,0 +1,20 @@
+main(){
+	
+	printf(" _________        .---\"\"\"      \"\"\"---.              \n");
+    printf(":______.-':      :  .--------------.  :             \n");
+    printf("| ______  |      | :                : |             \n");
+    printf("|:______B:|      | |  LUDO GAME !   | |             \n");
+    printf("|:______B:|      | |                | |             \n");
+    printf("|:______B:|      | |  1.NovoJogo    | |             \n");
+    printf("|         |      | |  2.NovoJogador | |             \n");
+    printf("|:_____:  |      | |  3.Rank        | |             \n");
+    printf("|    ==   |      | :  4. Sair       : |             \n");
+    printf("|       O |      :  '--------------'  :             \n");
+    printf("|       o |      :'---...______...---'              \n");
+    printf("|       o |-._.-i___/'             \\._              \n");
+    printf("|'-.____o_|   '-.   '-...______...-'  `-._          \n");
+    printf(":_________:      `.____________________   `-.___.-. \n");
+    printf("                 .'.eeeeeeeeeeeeeeeeee.'.      :___:\n");
+    printf("               .'.eeeeeeeeeeeeeeeeeeeeee.'.         \n");
+    printf("              :____________________________:\n");
+}
---
+++ Telas/PRONTOS/rank.c
@@ -0,0 +1,25 @@
+main(){
+	
+	printf("POSICAO		NOME DO JOGADOR		QTD DE PONTOS			\n\n");
+ 				  	
+   printf("1   		MURILO                  18\n");			
+   printf("2		GUILHERME		15\n");		
+   printf("3		LEONARDO		13\n");		
+   printf("4		EDUARDO			9\n");
+   printf("5		RAFAEL			3\n\n\n");
+
+printf("TABELA DE PONTOS:\n");
+printf("1 = 5 PONTOS\n");	
+printf("2 = 3 PONTOS\n");
+printf("3 = 2 PONTOS\n");
+printf("4 = 1 PONTO\n");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
