13c1b1f8f7d4ec7fb769d27759e531faae53ce39
João Vitor da Silva <joaovsilva3530@gmail.com>
2024-04-04 20:58:07 -0300

    .exe criado

--- main.c
+++ main.c
@@ -80,8 +80,8 @@ menu_principal:
 		printf("|		####################                 |\n");
 		printf("|                                                    |\n");
 		printf("|               1. 1  JOGADOR                        |\n");
-		printf("|               2. 2  JOGADOR                        |\n");
-		printf("|               3. 3  JOGADOR                        |\n");
+		printf("|               2. 2  JOGADORES                      |\n");
+		printf("|               3. 3  JOGADORES                      |\n");
 		printf("|               4. 4. JOGADORES                      |\n");
 		printf("|               5. Voltar                            |\n");
 		printf("------------------------------------------------------\n");
